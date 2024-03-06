10e453d4092a695bef68d0383c21a72cf24627d0
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2024-03-06 22:59:07 +0200

    update license

--- bedrock/bedrock.go
+++ bedrock/bedrock.go
@@ -3,7 +3,7 @@
 //
 // This file may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
-// https://github.com/kshard/embeddings
+// https://github.com/kshard/chatter
 //
 
 package bedrock

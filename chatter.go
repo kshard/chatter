d19d231d6651553c009d5f9c68178315b478c227
fogfish <dmkolesnikov@gmail.com>
2024-11-08 22:16:35 +0200

    Use encoding.TextMarshaler as prompt abstraction (#10)
    * Use encoding.TextMarshaler as prompt abstraction

--- chatter.go
+++ chatter.go
@@ -8,12 +8,16 @@
 
 package chatter
 
-import "context"
+import (
+	"context"
+	"encoding"
+)
 
 // The generic trait to "interact" with LLMs;
 type Chatter interface {
-	ConsumedTokens() int
-	Prompt(context.Context, *Prompt, ...func(*Options)) (string, error)
+	UsedInputTokens() int
+	UsedReplyTokens() int
+	Prompt(context.Context, encoding.TextMarshaler, ...func(*Options)) (string, error)
 }
 
 type Options struct {
@@ -29,21 +33,26 @@ type Options struct {
 	// number of choices to avoid overly diverse or nonsensical outputs while
 	// maintaining diversity within the top-ranked options.
 	TopP float64
+
+	// Token quota for reply, the model would limit response given number
+	Quota int
 }
 
-const DefaultTemperature = 0.5
+// Return default options chatter
+func NewOptions() Options {
+	return Options{
+		Temperature: 0.5,
+		TopP:        0.9,
+	}
+}
 
 // LLMs' critical parameter influencing the balance between predictability
 // and creativity in generated text. Lower temperatures prioritize exploiting
 // learned patterns, yielding more deterministic outputs, while higher
 // temperatures encourage exploration, fostering diversity and innovation.
 func WithTemperature(t float64) func(*Options) {
-	return func(opt *Options) {
-		opt.Temperature = t
-	}
+	return func(opt *Options) { opt.Temperature = t }
 }
-
-const DefaultTopP = 0.9
 
 // Nucleus Sampling, a parameter used in LLMs, impacts token selection by
 // considering only the most likely tokens that together represent
@@ -51,7 +60,10 @@ const DefaultTopP = 0.9
 // number of choices to avoid overly diverse or nonsensical outputs while
 // maintaining diversity within the top-ranked options.
 func WithTopP(p float64) func(*Options) {
-	return func(opt *Options) {
-		opt.TopP = p
-	}
+	return func(opt *Options) { opt.TopP = p }
+}
+
+// Limit reply to given quota
+func WithQuota(quota int) func(*Options) {
+	return func(opt *Options) { opt.Quota = quota }
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/kshard/chatter
 
-go 1.21.0
+go 1.23

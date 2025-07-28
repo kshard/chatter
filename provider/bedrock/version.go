0c759ca34de12032e4bfe694a3af52f51727cb94
fogfish <dmkolesnikov@gmail.com>
2025-07-28 11:26:02 +0300

    release providers v0.10.0 (#56)

---
+++ provider/bedrock/version.go
@@ -0,0 +1,11 @@
+//
+// Copyright (C) 2024 - 2025 Dmitry Kolesnikov
+//
+// This file may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+// https://github.com/kshard/chatter
+//
+
+package bedrock
+
+const Version = "provider/bedrock/v0.10.0"
